5af22c82305bd5907672580d5afe2b9beff8d6b2
mikiswahn <miki.swahn@gmail.com>
2017-11-01 09:43:59 +0100

    skel

---
+++ CPP.cs
@@ -0,0 +1,2 @@
+//the BNF grammar file
+
---
+++ parser.hs
@@ -0,0 +1,16 @@
+-- implement via CPP.cs file and bnfc tool
+-- rules for Programs
+
+-- rules for Definitions
+
+-- rules for Argument lists, declarations, and function bodies,
+
+-- rules for Statements
+
+-- rules for Expressions
+
+-- rules for Qualified constants
+
+-- rules for Types
+
+-- rules for Literals
