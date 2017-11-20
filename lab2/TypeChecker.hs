81f80fb02986974aaac719c572691a8dc323b46a
johanhyrefeldt <33259045+johanhyrefeldt@users.noreply.github.com>
2017-11-20 17:49:35 +0100

    Added function inferMon and edited inferExp.

--- lab2/TypeChecker.hs
+++ lab2/TypeChecker.hs
@@ -9,6 +9,103 @@ import CPP.Abs
 import CPP.Print
 import CPP.ErrM
 
+type Env = (Sig,[Context])      -- functions and context stack
+type Sig = Map Id ([Type],Type) -- function type signature
+type Context = Map Id Type      -- variables with their types
 
 typecheck :: Program -> Err ()
-typecheck p = return ()
+typecheck (Prog defs) = return ()
+
+checkDef :: 
+
+inferExp :: Env -> Exp -> Err Type
+inferExp env x = case x of
+    ETrue  -> return Type_bool
+    EFalse -> return Type_bool
+    EInt n -> return Type_int
+    EDouble m -> return Type_double
+    EId id -> lookupVar env id
+    EAdd exp1 exp2 ->
+        inferBin [Type_int, Type_double] env exp1 exp2
+    EMinus exp1 exp2 ->
+        inferBin [Type_int, Type_double] env exp1 exp2
+    ETimes exp1 exp2 ->
+        inferBin [Type_int, Type_double] env exp1 exp2
+    EDiv exp1 exp2 ->
+        inferBin [Type_int, Type_double] env exp1 exp2
+    EPostIncr exp ->
+        inferMon [Type_int, Type_double] env exp
+    EPostDecr exp ->
+        inferMon [Type_int, Type_double] env exp
+    EPreIncr exp ->
+        inferMon [Type_int, Type_double] env exp
+    EPreDecr exp ->
+        inferMon [Type_int, Type_double] env exp
+
+inferMon :: [Type] -> Env -> Exp -> Err Type
+inferMon types env exp = do
+    typ <- inferExp env exp
+        if elem typ types
+            then
+                return typ
+            else
+                fail $ "wrong type of expression " ++ printTree exp
+
+inferBin :: [Type] -> Env -> Exp -> Exp -> Err Type
+inferBin types env exp1 exp2 = do
+    typ <- inferExp env exp1
+    if elem typ types
+        then
+            checkExp env exp2 typ
+        else
+            fail $ "wrong type of expression " ++ printTree exp1
+
+checkExp :: Env -> Type -> Exp -> Err ()
+checkExp env typ exp = do
+    typ2 <- inferExp env exp
+    if (typ2 = typ) then
+        return ()
+    else
+        fail $ "type of " ++ printTree exp ++
+        "expected " ++ printTree typ ++
+        "but found " ++ printTree typ2
+
+checkStm :: Env -> Type -> Stm -> Err Env
+checkStm env val x = case x of
+    SExp exp -> do
+        inferExp env exp
+        return exp
+    SDecl typ x ->
+        updateVar env id typ
+    SWhile exp stm -> do
+        checkExp env Type_bool exp
+        checkStm env val stm
+
+checkStms :: Env -> [Stm] -> Err Env
+checkStms env stms = case stms of
+    [] -> return env
+    x : rest -> do 
+        env' <- checkStm env x
+        checkStms env' rest
+
+-- This doesn't correspond to: type Env = (Sig,[Context])
+emptyEnv  :: Env
+emptyEnv = [[]]
+
+newBlock  :: Env -> Env
+newBlock env = []:env
+
+lookupVar :: Env -> Id -> Err Type
+lookupVar [] x = fail $ "Unknown variable " ++ printTree x ++ "."
+lookupVar (scope:rest) x = case lookup x scope of
+                             Nothing -> lookupVar rest x
+                             Just t  -> return t
+
+updateVar :: Env -> Id -> Type -> Err Env
+updateVar (scope:rest) x t = 
+    case lookup x scope of
+      Nothing -> return (((x,t):scope):rest)
+      Just _  -> fail ("Variable " ++ printTree x ++ " already declared.")
+
+lookupFun :: Env -> Id -> Err ([Type],Type)
+updateFun :: Env -> Id -> ([Type],Type) -> Err Env
