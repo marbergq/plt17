e2be1a001d58a9da5659af918b7deb4ae7d64435
mikiswahn <miki.swahn@gmail.com>
2017-11-29 14:28:55 +0100

    covered statements from grammar

--- lab2/TypeChecker.hs
+++ lab2/TypeChecker.hs
@@ -9,87 +9,59 @@ import CPP.Abs
 import CPP.Print
 import CPP.ErrM
 
--- TYPE CHECKER --
-
---type Env = [[(Id, Type)]]
 type Sig     = Map Id ([Type], Type)
-type Context = Map Id Type  --a.k.a. env blocks
+type Context = Map Id Type
 type Env     = (Sig, [Context])
 
---typecheck :: Program -> Err ()
---typecheck (Prog stms) = checkStms starterEnv stms
-typecheck :: Program -> Err Sig
-typecheck (PDefs defs) = Ok $ listFunDefs starterSig defs
---typecheck :: Program -> Err ()
---typecheck (PDefs defs) = do
---  checkDefs (listFunDefs emptySig defs, [emptyContext]) defs
+typecheck :: Program -> Err ()
+typecheck (PDefs defs) = checkDefs ((addDefs starterSig defs), [Map.empty]) defs
 
-listFunDefs :: Sig -> [Def] -> Sig
-listFunDefs sigs []                         = sigs
-listFunDefs sigs ( (DFun t f args _):funs ) =
-    listFunDefs (Map.insert f 
-                            ((listTypes [] args), t)
-                            sigs
-                 )
-                 funs
+addDefs :: Sig -> [Def] -> Sig
+addDefs sigs []                         = sigs
+addDefs sigs ( (DFun t f args _):funs ) =
+    addDefs (Map.insert f ((enlistTypes [] args), t) sigs ) funs
 
-listTypes :: [Type] -> [Arg] -> [Type]
-listTypes list []                 = list
-listTypes list ((ADecl t _):args) = listTypes (t:list) args
+enlistTypes :: [Type] -> [Arg] -> [Type]
+enlistTypes list []                 = list
+enlistTypes list ((ADecl t _):args) = enlistTypes (t:list) args
 
---checkDefs :: Env -> [Def] -> Err ()
---checkDefs env [] = return ()
---checkDefs env (def:defs) = do checkDef env def
---                              checkDefs env defs
---
---checkDef :: Env -> Def -> Err ()
----- Maybe add a new scope.
----- The return type doesn't match with addArgs.
---checkDef env (DFun t f args stms) = case addArgs env args of
---                                      Bad err -> fail err
---                                      Ok env' -> checkStms env' stms
---
---addArgs :: Env -> [Arg] -> Err Env
---addArgs env [] = Ok env
-----addArgs env ((ADecl t id):args) = addArgs (addVar env id t) args
---addArgs env ((ADecl t id):args) = case addVar env id t of
---                                    Bad err -> fail err
---                                    Ok env' -> addArgs env' args
+checkDefs :: Env -> [Def] -> Err ()
+checkDefs env [] = return ()
+checkDefs env (def:defs) = case checkDef env def of
+                              Bad err -> fail err
+                              Ok _ -> checkDefs env defs
+
+checkDef :: Env -> Def -> Err ()
+checkDef env (DFun t f args stms) = case addArgs (addScope env) args of
+                                      Bad err -> fail ("In function " ++ show f ++ ":\n" ++ err)
+                                      Ok env' -> checkStms env' stms
+
+addArgs :: Env -> [Arg] -> Err Env
+addArgs env [] = Ok env
+addArgs env ((ADecl t id):args) = case addVar env [id] t of
+                                    Bad err -> fail ("Vairable declared twice in argument list.\n" ++ err)
+                                    Ok env' -> addArgs env' args
 
 checkStms :: Env -> [Stm] -> Err ()
 checkStms env []        = return ()
 checkStms env (st:stms) = do env' <- checkStm env st
                              checkStms env' stms
 
---checkStms :: Env -> [Stm] -> Err Env
---checkStms env stms = case stms of
---    [] -> return env
---    x : rest -> do 
---        env' <- checkStm env x
---        checkStms env' rest
-
+--Statements 
 checkStm :: Env -> Stm -> Err Env
 checkStm env s = 
     case s of
-      SDecl t x       -> addVar env x t
-      SAss x e        -> do t <- lookupVar env x
-                            checkExp env e t
-                            return env      
+      SDecls t xs       -> addVar env xs t
+      SInit t id e    -> do t <- inferExp env e
+                            addVar env [id] t
       SBlock stms     -> do checkStms (addScope env) stms
                             return env
       SReturn e       -> do inferExp env e
                             return env
-
---checkStm :: Env -> Type -> Stm -> Err Env
---checkStm env val x = case x of
---    SExp exp -> do
---        inferExp env exp
---        return exp
---    SDecl typ x ->
---        updateVar env id typ
---    SWhile exp stm -> do
---        checkExp env Type_bool exp
---        checkStm env val stm
+      SExp e          -> do inferExp env e
+                              return env
+      --SWhile e stm    ->
+      --SIfElse e stm stm ->
 
 checkExp :: Env -> Exp -> Type -> Err ()
 checkExp env e t = 
@@ -99,58 +71,68 @@ checkExp env e t =
                     ++ " expected " ++ printTree t)
          else return ()
 
---checkExp :: Env -> Type -> Exp -> Err ()
---checkExp env typ exp = do
---    typ2 <- inferExp env exp
---    if (typ2 = typ) then
---        return ()
---    else
---        fail $ "type of " ++ printTree exp ++
---        "expected " ++ printTree typ ++
---        "but found " ++ printTree typ2
-
+-- Expresions 
 inferExp :: Env -> Exp -> Err Type
-inferExp env e = 
+inferExp env e =
     case e of
-      EId x          -> lookupVar env x
       EInt _         -> return Type_int
       EDouble _      -> return Type_double
-    --  ETrue          -> return Type_bool
-    --  EFalse         -> return Type_bool
-    --  ?              -> return Type_void
-      EAdd e1 e2     -> do t1 <- inferExp env e1
-                           t2 <- inferExp env e2
-                           if t1 == t2 
-                             then return t1
-                             else fail (printTree e1 ++ " has type " 
-                                         ++ printTree t1 ++ " but " 
-                                         ++ printTree e2 ++ " has type " 
-                                         ++ printTree t2)
-    --  EAdd exp1 exp2 -> inferBin 
-    --                    [Type_int, Type_double, Type_string]
-    --                    env exp1 exp2
+      ETrue          -> return Type_bool
+      EFalse         -> return Type_bool
+      EId x          -> lookupVar env x
+      EApp f es      -> lookupFun env f es
+      EPlus e1 e2    -> inferBinary [Type_int, Type_double] env e1 e2
+      EMinus e1 e2   -> inferBinary [Type_int, Type_double] env e1 e2
+      ETimes e1 e2   -> inferBinary [Type_int, Type_double] env e1 e2
+      EDiv e1 e2     -> inferBinary [Type_int, Type_double] env e1 e2
+      EAss id e      -> do t <- lookupVar env id
+                           checkExp env e t
+                           return t
+      EOr e1 e2      -> inferBinary [Type_bool] env e1 e2
+      EAnd e1 e2     -> inferBinary [Type_bool] env e1 e2
+      EEq e1 e2      -> inferBinary [Type_bool] env e1 e2
+      ENEq e1 e2     -> inferBinary [Type_bool] env e1 e2
+      EELt. e1 e2    -> inferBinary [Type_bool] env e1 e2
+      EEGt. e1 e2    -> inferBinary [Type_bool] env e1 e2
+      EELtEq. e1 e2  -> inferBinary [Type_bool] env e1 e2
+      EEGtEq. e1 e2  -> inferBinary [Type_bool] env e1 e2
+      EPostIncr. x   -> lookupVar env x
+      EPostDecr. x   -> lookupVar env x
+      EPreIncr. x    -> lookupVar env x
+      EPreDecr. x    -> lookupVar env x 
+
+inferBinary :: [Type] -> Env -> Exp -> Exp -> Err Type
+inferBinary validTypes env e1 e2 = do
+    t1 <- inferExp env e1
+    if elem t1 validTypes
+        then
+            case checkExp env e2 t of
+                Bad err -> fail (printTree e1 ++ " has type " ++ printTree t1
+                                 ++ " whereas " ++ err)
+                Ok _ -> return t1
+        else
+            fail $ "Cannot apply function on type " ++ printTree t1
 
 starterEnv :: Env
 starterEnv = (starterSig, [])
 
 starterSig :: Sig
-starterSig = --Map.insert printInt ([Type_int],Type_void) $
-           --Map.insert printDouble ([Type_bool],Type_void) $
-           --Map.insert readInt ([],Type_int) $
-          -- Map.insert readDouble ([], Type_bool) 
-          (Map.empty)
-
-starterContext :: Context
-starterContext = Map.empty
+starterSig = Map.insert (Id "printInt") ([Type_int],Type_void) $
+             Map.insert (Id "printDouble") ([Type_bool],Type_void) $
+             Map.insert (Id "readInt") ([],Type_int) $
+             Map.insert (Id "readDouble") ([], Type_bool) 
+             (Map.empty)
 
 --scope also refered to as context
+
 addScope :: Env -> Env
 addScope (sig, scopes) = (sig, (Map.empty):scopes)
 
-addVar :: Env -> Id -> Type -> Err Env
-addVar (sig, scope:rest) x t = 
+addVar :: Env -> [Id] -> Type -> Err Env
+addVar env [] _ = return env
+addVar (sig, scope:rest) x:xs t = 
     case Map.lookup x scope of
-      Nothing -> return (sig, ((Map.insert x t scope):rest))
+      Nothing -> addVar (sig, ((Map.insert x t scope):rest)) xs t
       Just _  -> fail ("Variable " ++ printTree x ++ " already declared.")
 
 lookupVar :: Env -> Id -> Err Type
@@ -163,3 +145,8 @@ lookupVar (sig, scope:rest) x = case Map.lookup x scope of
 --lookupFun (sigs, cons) x = case Map.lookup x sigs of
 --                            Nothing -> fail $ "Unknown function " ++ printTree x ++ "."
 --                            Just t -> return t
+
+lookupFun :: Env -> Id -> [Exp] -> Err Type
+lookupFun (sig, scope:rest) f e:es = case Map.lookup f sig of
+                          Just (_, t) -> return t
+                          Nothing     -> fail ("Function " ++ show f ++ "not declared")
