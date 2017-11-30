e167f7efd88bd837d227159893c193794fdcf969
mikiswahn <miki.swahn@gmail.com>
2017-11-30 10:32:38 +0100

    merege ready

--- lab2/TypeChecker.hs
+++ lab2/TypeChecker.hs
@@ -1,8 +1,3 @@
-{-
-This is our beginning of a type checker. It is not finished and doesn't work
-yet.
--}
-
 module TypeChecker where
 
 import Control.Monad
@@ -14,27 +9,23 @@ import CPP.Abs
 import CPP.Print
 import CPP.ErrM
 
-type Env = (Sig,[Context])      -- functions and context stack
-type Sig = Map Id ([Type],Type) -- function type signature
-type Context = Map Id Type      -- variables with their types
+type Env = (Sig,[Context])
+type Sig = Map Id ([Type],Type)
+type Context = Map Id Type
 
 typecheck :: Program -> Err ()
 typecheck (PDefs defs) =
-  checkDefs (listFunDefs starterSig defs, [Map.empty]) defs
+  checkDefs (addDefs starterSig defs, [Map.empty]) defs
 
 -- This function initializes the signature symbol table
-listFunDefs :: Sig -> [Def] -> Sig
-listFunDefs sigs []                         = sigs
-listFunDefs sigs ( (DFun t f args _):funs ) =
-    listFunDefs (Map.insert f 
-                            ((reverse (listTypes [] args)), t)
-                            sigs
-                 )
-                 funs
+addDefs :: Sig -> [Def] -> Sig
+addDefs sigs []                         = sigs
+addDefs sigs ( (DFun t f args _):funs ) =
+    addDefs (Map.insert f ((reverse (enlistTypes [] args)), t) sigs) funs
 
-listTypes :: [Type] -> [Arg] -> [Type]
-listTypes list []               = list
-listTypes list ((ADecl t _):args) = listTypes (t:list) args
+enlistTypes :: [Type] -> [Arg] -> [Type]
+enlistTypes list []               = list
+enlistTypes list ((ADecl t _):args) = enlistTypes (t:list) args
 
 checkDefs :: Env -> [Def] -> Err ()
 checkDefs env [] = return ()
@@ -47,10 +38,6 @@ checkDef env (DFun t f args stms) = case addArgs (addScope env) args of
                                       Bad err -> fail ("In function " ++ (show f) ++ ": " ++ err)
                                       Ok env' -> do checkStms env' stms f
                                                     return ()
--- checkDef env (DFun t f args stms) = do env' <- addVar env [f] t
---                                        case addArgs (addScope env') args of
---                                          Bad err -> fail ("In function " ++ (show f) ++ ": " ++ err)
---                                          Ok env'' -> checkStms env'' stms
 
 addArgs :: Env -> [Arg] -> Err Env
 addArgs env [] = Ok env
@@ -58,6 +45,7 @@ addArgs env ((ADecl t id):args) = case addVar env [id] t of
                                     Bad err -> fail ("Variable declared twice in argument list. " ++ err)
                                     Ok env' -> addArgs env' args
 
+--Statements
 checkStm :: Env -> Stm -> Id -> Err Env
 checkStm env s fid = 
     case s of
@@ -80,33 +68,13 @@ checkStm env s fid =
                               checkStm env s1 fid
                               checkStm env s2 fid
 
-
-
--- checkStm :: Env -> Type -> Stm -> Err Env
--- checkStm env val x = case x of
---     SExp exp -> do
---         inferExp env exp
---         return exp
---     SDecl typ x ->
---         updateVar env id typ
---     SWhile exp stm -> do
---         checkExp env Type_bool exp
---         checkStm env val stm
-
 checkStms :: Env -> [Stm] -> Id -> Err Env
 checkStms env [] _ = return env
 checkStms env (stm:stms) fid = do env' <- checkStm env stm fid
                                   checkStms env' stms fid
 
--- checkStms :: Env -> [Stm] -> Err Env
--- checkStms env stms = case stms of
---     [] -> return env
---     x : rest -> do 
---         env' <- checkStm env x
---         checkStms env' rest
-
 -- Expressions 
--- NTS: I would like to try to use printTree e in the case block
+-- Johan NTS: I would like to try to use printTree e in the case block
 inferExp :: Env -> Exp -> Err Type
 inferExp env e =
     case e of
@@ -120,7 +88,6 @@ inferExp env e =
                              Bad err -> fail ("Illegal function call "
                                               ++ printTree f ++ ". " ++ err)
                              Ok _    -> return (snd funType)
-                           -- map (\p -> checkExp env (fst p) (snd p)) (zip es (fst funType)) 
 
       EPlus e1 e2    -> inferBinary [Type_int, Type_double] env e1 e2
       EMinus e1 e2   -> inferBinary [Type_int, Type_double] env e1 e2
@@ -155,40 +122,6 @@ inferExp env e =
                             then return t
                             else fail "Wrong type in decrement expression."
 
--- inferExp :: Env -> Exp -> Err Type
--- inferExp env x = case x of
---     ETrue  -> return Type_bool
---     EFalse -> return Type_bool
---     EInt n -> return Type_int
---     EDouble m -> return Type_double
---     EId id -> lookupVar env id
---     EAdd exp1 exp2 ->
---         inferBin [Type_int, Type_double] env exp1 exp2
---     EMinus exp1 exp2 ->
---         inferBin [Type_int, Type_double] env exp1 exp2
---     ETimes exp1 exp2 ->
---         inferBin [Type_int, Type_double] env exp1 exp2
---     EDiv exp1 exp2 ->
---         inferBin [Type_int, Type_double] env exp1 exp2
---     EPostIncr exp ->
---         inferMon [Type_int, Type_double] env exp
---     EPostDecr exp ->
---         inferMon [Type_int, Type_double] env exp
---     EPreIncr exp ->
---         inferMon [Type_int, Type_double] env exp
---     EPreDecr exp ->
---         inferMon [Type_int, Type_double] env exp
---     -- More steps to be added here.
-
--- inferMon :: [Type] -> Env -> Exp -> Err Type
--- inferMon types env exp = do
---     typ <- inferExp env exp
---         if elem typ types
---             then
---                 return typ
---             else
---                 fail $ "wrong type of expression " ++ printTree exp
-
 inferBinary :: [Type] -> Env -> Exp -> Exp -> Err Type
 inferBinary validTypes env e1 e2 = do
     t1 <- inferExp env e1
@@ -218,27 +151,16 @@ checkArgs env (e:es) (t:ts) = case checkExp env e t of
                                 Bad err -> fail $ "In argument list: " ++ err
                                 Ok _    -> checkArgs env es ts
 
--- checkExp :: Env -> Type -> Exp -> Err ()
--- checkExp env typ exp = do
---     typ2 <- inferExp env exp
---     if (typ2 = typ) then
---         return ()
---     else
---         fail $ "type of " ++ printTree exp ++
---         "expected " ++ printTree typ ++
---         "but found " ++ printTree typ2
-
-
 starterEnv :: Env
 starterEnv = (starterSig, [])
 
+--returns a list of signatures including built in funs
 starterSig :: Sig
 starterSig = Map.insert (Id "printInt") ([Type_int],Type_void) $
              Map.insert (Id "printDouble") ([Type_bool],Type_void) $
              Map.insert (Id "readInt") ([],Type_int) $
              Map.insert (Id "readDouble") ([], Type_bool) 
              (Map.empty)
---Probably add the built-in functions. Either here or in listFunDefs.
 
 addVar :: Env -> [Id] -> Type -> Err Env
 addVar env [] _ = return env
@@ -260,8 +182,3 @@ lookupFun (sigs, cons) x = case Map.lookup x sigs of
 
 addScope :: Env -> Env
 addScope (sigs, cons) = (sigs, (Map.empty):cons)
-
--- This function is maybe unnecessary since listFunDefs
--- does the job in the beginning.
-addSig :: Env -> Id -> [Type] -> Type -> Env
-addSig (sigs,cons) f args ret = (Map.insert f (args, ret) sigs, cons)
