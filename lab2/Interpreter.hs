df40a407a7380fb49bda06599c1532f4f16d0f1b
mikiswahn <miki.swahn@gmail.com>
2018-01-02 10:17:25 +0100

    aligned function application with text book

--- lab2/Interpreter.hs
+++ lab2/Interpreter.hs
@@ -69,42 +69,33 @@ execStms env (st:stms) = case lookupVar env (Id "ret_val'") of
                                         execStms env' stms
                            val    -> return (env, val)
 
+--Consider removing Value in return type. /m
 execStm :: Env -> Stm -> IO (Env, Value)
 execStm env s = 
     case s of
-      SExp e          -> evalExp env e
-      SDecls _ []     -> return (env, VVoid)
-      SDecls t (id:ids) -> execStm (addVar env id) (SDecls t ids)
-
+      SExp e             -> evalExp env e
+      SDecls _ []        -> return (env, VVoid)
+      SDecls t (id:ids)  -> execStm (addVar env id) (SDecls t ids)
       -- Change: Pass env' (output from evalExp) to addVar according to rule in
       -- the PLT textbook. /Johan
-      SInit _ id e     -> do (env', val) <- evalExp env e
-                             return (setVar (addVar env' id) id val, VVoid)
-      
+      SInit _ id e       -> do (env', val) <- evalExp env e
+                               return (setVar (addVar env' id) id val, VVoid)
       -- Encountering SReturn changes the variable ret_val' from VUndef to the
       -- value of expression e.
       -- ret_val' with a defined value is caught by the function execStms by
       -- pattern matching /Johan
-      SReturn e       -> do (env', val) <- (evalExp env e)
-                            return ((setVar env' (Id "ret_val'") val), VVoid)
-
-      -- I wonder if it is correct to leave the pop the topmost scope and add a
-      -- new one everytime we enter a new iteration. Maybe it is... /Johan
-      SWhile eCon s   -> do (env', VBool b) <- evalExp env eCon
-                            if (b == False)
-                              then return (env', VVoid)
-                              else case s of
-                                SBlock _ -> do (env'', _) <- execStm env' s
-                                               execStm env'' (SWhile eCon s)
-                                -- When the body is a single statement. /Johan
-                                _ -> do (env'', _) <- execStm (enterScope env') s
-                                        execStm (leaveScope env'') (SWhile eCon s)
-      --enter scope in first iteration of while loop and exit scope after last iteration
-      -- is that what we want to do? /Johan
-
-      SBlock stms     -> do (env', _) <- execStms (enterScope env) stms
-                            return (leaveScope env', VVoid)
-      
+      SReturn e          -> do (env', val) <- (evalExp env e)
+                               return ((setVar env' (Id "ret_val'") val), VVoid)
+      SWhile eCon s      -> do (env', VBool b) <- evalExp env eCon
+                               if (b == False)
+                                 then return (env', VVoid)
+                                 else case s of
+                                   SBlock _ -> do (env'', _) <- execStm env' s
+                                                  execStm env'' (SWhile eCon s)
+                                   _ -> do (env'', _) <- execStm (enterScope env') s
+                                           execStm (leaveScope env'') (SWhile eCon s)
+      SBlock stms        -> do (env', _) <- execStms (enterScope env) stms
+                               return (leaveScope env', VVoid)
       SIfElse eCon sI sE -> do (env', VBool b) <- evalExp env eCon
                                if (b == True)
                                   then case sI of
@@ -119,26 +110,19 @@ execStm env s =
 evalExp :: Env -> Exp -> IO (Env, Value)
 evalExp env e = 
     case e of
-      -- Maybe "true"/"false" is needed after ETrue/EFalse or just _ /Johan
       ETrue          -> return (env, VBool True)
       EFalse         -> return (env, VBool False)
       EInt i         -> return (env, VInt i)
       EDouble d      -> return (env, VDouble d)
-      EId id          -> return (env, lookupVar env id)
+      EId id         -> return (env, lookupVar env id)
       EApp f xs      -> do (DFun _ _ args stms) <- return (lookupFun env f)
                            -- Create variable ret_val' (description above). /Johan
-                           env' <- setArgs (addVar (enterScope env) (Id "ret_val'")) env xs args
-                           (env'', val) <- execStms env' stms
-                           return ((leaveScope env''), val) 
-                           --vilket vädre ska funktionsanrop retunera? (t,_) i args. 
-                           --hur får man det från execStms? 
-
-                           -- The execStms has now been designed to give the
-                           -- return value if a return statement has been
-                           -- encountered. /Johan
-
+                           (freshEnv, oldEnv') <- setArgs (addVar (enterScope env) (Id "ret_val'")) env xs args
+                           (_, val) <- execStms freshEnv stms
+                           return (oldEnv', val) 
       -- The four built-in functions can be hard coded as special cases of EApp
       -- in this function evalExp (according to labPM).
+      --agreed. /m
 
       --EPostIncr
       --EPostDecr
@@ -172,28 +156,16 @@ starterEnv = (starterSig, [Map.empty])
 
 starterSig :: Sig
 starterSig = Map.empty
---starterSig = Map.insert (Id "printInt")
---                         (DFun Type_void (Id "printInt")
---                          [ADecl Type_int (Id "x")] [???]) $
---             --Map.insert (Id "printDouble") ([Type_bool],Type_void) $
---             --Map.insert (Id "readInt") ([],Type_int) $
---             --Map.insert (Id "readDouble") ([], Type_bool) 
---             (Map.empty)
 
 addVar :: Env -> Id -> Env
--- I implemented the solution from addSetVar to check if the variable is already
--- declared. /Johan
 addVar (sigs, (scope:rest)) id = case Map.lookup id scope of 
     Just _  -> error $ "Variable " ++ printTree id ++ " already declared"
     Nothing -> (sigs, ((Map.insert id VUndef scope):rest))
 
 -- DOES NOT HAVE RETURN TYPE FOR ERRORS
+-- Only to be used for declared variables.
 setVar :: Env -> Id -> Value -> Env
 setVar (_, []) id _ = error $ "Unknown variable " ++ printTree id ++ "."
--- This case is probably not needed when we use Data.Map
--- setVar (sigs, ((Map.empty):rest)) id v = let (sigs', rest') = setVar (sigs, rest) id v
---                                           in (sigs', (Map.empty):rest')
--- The current context is not empty -> look for the variable and update if found.
 setVar (sigs, (scope:rest)) id v = case Map.lookup id scope of
     Just _  -> (sigs, (Map.insert id v scope):rest)
     Nothing -> let (sigs', rest') = setVar (sigs, rest) id v
@@ -212,11 +184,11 @@ fresh scope.
 The second argument is the environment in which the argument expressions are
 evaluated. This environment should not have a fresh scope on top.
 -}
-setArgs :: Env -> Env -> [Exp] -> [Arg] -> IO Env
-setArgs env env' [] [] = return env
-setArgs env env' (e:es) ((ADecl _ id):args) = do
-  (env'', v) <- evalExp env' e
-  setArgs (setVar (addVar env id) id v) env'' es args
+setArgs :: Env -> Env -> [Exp] -> [Arg] -> IO (Env, Env)
+setArgs freshEnv oldEnv' [] []                     = return (freshEnv, oldEnv')
+setArgs freshEnv oldEnv (e:es) ((ADecl _ id):args) = do
+  (oldEnv', v) <- evalExp oldEnv e --oldEnv' captures potential side effects
+  setArgs (addSetVar freshEnv id v) oldEnv' es args
 
 -- DOES NOT HAVE RETURN TYPE FOR ERRORS
 -- lookupVar can output the value VUndef.
